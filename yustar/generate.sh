419d56a950a8c7dab09aa7b3bfa82f0ef1d8fcaa
Yubao Liu <yubao.liu@gmail.com>
2024-07-27 11:04:17 +0800

    upgrade to yustar-3.6.0

--- yustar/generate.sh
+++ yustar/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Usage: ./generate.sh path/to/yuhao_star_v3.5.0/schema
+# Usage: ./generate.sh path/to/yuhao_star_v3.6.0/schema
 
 set -euo pipefail
 
@@ -18,11 +18,11 @@ perl -CSDA -Mutf8 -i -pE 's/^({烏上}\s+D)\?/\1w/' roots.tsv
     { cat roots-mapping-error.txt; exit 1; }
 
 perl -CSDA -lnE 'print "$1.dict.yaml" if (/^import_tables/ .. /^\S(?!mport)/) && /^\s*-\s+(.\S+)/' "$1/yustar.dict.yaml" |
-    sed -e "s|^|$1|" |
+    sed -e "s|^|$1/|" |
     xargs perl -CSDA -lnE 'print if (/^\.\.\./ .. eof) && !/^\.\.\./ && !/^\s*$/' > mabiao.tsv
 
 perl -CSDA -lnE 'print "$1.dict.yaml" if (/^import_tables/ .. /^\S(?!mport)/) && /^\s*-\s+(.\S+)/' "$1/yustar_sc.dict.yaml" |
-    sed -e "s|^|$1|" |
+    sed -e "s|^|$1/|" |
     xargs perl -CSDA -lnE 'print if (/^\.\.\./ .. eof) && !/^\.\.\./ && !/^\s*$/' > mabiao_sc.tsv
 
 perl -CSDA -lne 'print "$1\t$2" if (/^\.\.\./ .. eof) && /^(\S+)\s+\[([^,]+)/' "$1"/yustar_chaifen{,_tw}.dict.yaml |
@@ -34,7 +34,7 @@ perl -CSDA -lne 'print "$1\t$2" if (/^\.\.\./ .. eof) && /^(\S+)\s+\[([^,]+)/' "
 cp -f "$1/../font/Yuniversus.ttf" .
 ../scripts/turn-roots-chaifen-mabiao-into-js.pl roots.tsv chaifen_sc.tsv mabiao_sc.tsv > yustar_sc.js
 ../scripts/generate-roots-chart.pl -u ../sbfd/ -e yustar_sc.js -r roots-mapping.tsv -f Yuniversus.ttf \
-    -t "宇浩星陳字根表 v3.5.0" \
+    -t "宇浩星陳字根表 v3.6.0" \
     roots.tsv chaifen_sc.tsv ../top6000.txt > yustar_sc.html
 
 perl -CSDA -i -pE 's/\r//' *.tsv *.csv
