9705dca720df1ce9493d53056043b372650bbeed
Yubao Liu <yubao.liu@gmail.com>
2025-05-14 08:27:05 +0800

    improve reverse engineering for yusm :-)

--- yusm/generate.sh
+++ yusm/generate.sh
@@ -5,8 +5,10 @@
 set -euo pipefail
 
 ./analyze-roots-of-yusm-input-method.pl "$1"/yusm_chaifen*.dict.yaml > roots.tsv
+perl -i -CSDA -Mutf8 -pE 's/^(\{曾中\}.*)/\1ⓘ/' roots.tsv
 
 ./analyze-roots-encoding-of-yusm.pl > encoding.tsv 2> roots2.tsv
+mv roots2.tsv roots.tsv
 
 perl -CSDA -lnE 'print "$1.dict.yaml" if (/^import_tables/ .. /^\S(?!mport)/) && /^\s*-\s+(.\S+)/' "$1/yusm_sc.dict.yaml" |
     sed -e "s|^|$1/|" |
@@ -21,4 +23,4 @@ perl -CSDA -lnE 'print "$1\t$2" if (/^\.\.\./ .. eof) && /^(\S+)\s+\[([^,]+)/' "
 
 ../scripts/generate-roots-chart.pl -u ../sbfd/ -e yusm.js \
     -t "宇浩日月字根表 v3.9.0-beta.20250512" \
-    roots2.tsv chaifen.tsv ../top6000.txt > yusm-v3.9.0-beta.20250512.html
+    roots.tsv chaifen.tsv ../top6000.txt > yusm-v3.9.0-beta.20250512.html
