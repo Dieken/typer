c4811cdd73ddc0f142233481101665c1643f18c7
Yubao Liu <yubao.liu@gmail.com>
2025-05-16 10:28:04 +0800

    宇浩日月_v3.9.0-beta.20250515

--- yusm/generate.sh
+++ yusm/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Usage: ./generate.sh path/to/宇浩日月_v3.9.0-beta.20250512/schema
+# Usage: ./generate.sh path/to/宇浩日月_v3.9.0-beta.20250515/schema
 
 set -euo pipefail
 
@@ -8,11 +8,13 @@ set -euo pipefail
 perl -i -CSDA -Mutf8 -pE 's/^(\{曾中\}.*)/\1ⓘ/' roots.tsv
 
 ./analyze-roots-encoding-of-yusm.pl > encoding.tsv 2> roots2.tsv
+grep ERROR roots2.tsv && echo "ERROR found in roots2.tsv" && exit 1
 mv roots2.tsv roots.tsv
 
 perl -CSDA -lnE 'print "$1.dict.yaml" if (/^import_tables/ .. /^\S(?!mport)/) && /^\s*-\s+(.\S+)/' "$1/yusm_sc.dict.yaml" |
     sed -e "s|^|$1/|" |
     xargs perl -CSDA -lnE 'print if (/^\.\.\./ .. eof) && !/^\.\.\./ && !/^\s*$/' |
+    fgrep -v ' ' |      # 去掉助记简码
     fgrep -v '～' > mabiao.tsv
 
 perl -CSDA -lnE 'print "$1\t$2" if (/^\.\.\./ .. eof) && /^(\S+)\s+\[([^,]+)/' "$1"/yusm_chaifen*.dict.yaml |
@@ -22,5 +24,5 @@ perl -CSDA -lnE 'print "$1\t$2" if (/^\.\.\./ .. eof) && /^(\S+)\s+\[([^,]+)/' "
 ../scripts/turn-roots-chaifen-mabiao-into-js.pl roots.tsv chaifen.tsv mabiao.tsv > yusm.js
 
 ../scripts/generate-roots-chart.pl -u ../sbfd/ -e yusm.js \
-    -t "宇浩日月字根表 v3.9.0-beta.20250512" \
-    roots.tsv chaifen.tsv ../top6000.txt > yusm-v3.9.0-beta.20250512.html
+    -t "宇浩日月字根表 v3.9.0-beta.20250515" \
+    roots.tsv chaifen.tsv ../top6000.txt > yusm-v3.9.0-beta.20250515.html
