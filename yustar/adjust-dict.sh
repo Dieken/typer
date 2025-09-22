f2be750830150e6e1f375d6ce79a6bd8b504a13a
Yubao Liu <yubao.liu@gmail.com>
2025-09-22 11:43:50 +0800

    yustar v3.10.1-beta.20250921

--- yustar/adjust-dict.sh
+++ yustar/adjust-dict.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export R=$HOME/tmp/input-method-analysis/yustar/星陳輸入法_v3.10.0/schema
+export R=$HOME/tmp/input-method-analysis/yustar/星陳輸入法_v3.10.1-beta.20250921/schema
 a=$(grep '^\s*-\s\+yuhao/' $R/yustar_sc.dict.yaml | perl -lpE 's/\r//; s/^\s*\-\s*/-d $ENV{R}\//; s/$/.dict.yaml/')
 ./adjust-dict.pl $a -c ../简体字频表-2.5b.txt -w ../词频数据.txt -c override_weight.txt -w override_weight.txt > yustar_sc.all.dict.yaml
 
