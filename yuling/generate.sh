57e6986dd9f45c9503164f62c3b66f50eb2ecd92
Yubao Liu <yubao.liu@gmail.com>
2026-01-20 17:35:05 +0800

    yuling v3.11.0-beta.20260120.082751

--- yuling/generate.sh
+++ yuling/generate.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 shopt -s failglob
 
-VER="v3.11.0-beta.20260119"
+VER="v3.11.0-beta.20260120"
 
 [ -e zigen-ling.csv ] || curl -LO 'https://github.com/forfudan/yu/raw/refs/heads/main/src/public/zigen-ling.csv'
 [ -e mabiao-ling.txt  ] || curl -LO 'https://github.com/forfudan/yu/raw/refs/heads/main/src/public/mabiao-ling.txt'
